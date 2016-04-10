751b9cb048d8ffd783af006d9fdc0187b405237d
Van Loi Bui <bvloi94@gmail.com>
2016-04-10 09:38:20 +0700

    complete authentication func

--- src/main/java/net/techeco/controller/Authentication.java
+++ src/main/java/net/techeco/controller/Authentication.java
@@ -3,9 +3,12 @@
  */
 package net.techeco.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import net.techeco.AjaxResponse;
 import net.techeco.dao.concrete.AccountDAO;
+import net.techeco.model.Account;
 import net.techeco.model.LoginCriteria;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 /**
  * @author Duy Beo
@@ -30,39 +34,44 @@ public class Authentication {
         return "Login";
     }
 
-    @RequestMapping(value = "Authenticate.htm", method = RequestMethod.POST, headers="Accept=*/*")
+    @RequestMapping(value = "Authenticate.htm", method = RequestMethod.POST, headers = "Accept=*/*", produces = "application/json")
     @ResponseBody
-    public String Login(HttpSession session, @RequestBody LoginCriteria account) {
+    public String Login(HttpSession session, @RequestBody LoginCriteria account) throws IOException {
 
         ObjectMapper mapper = new ObjectMapper();
         ObjectNode node = mapper.createObjectNode();
 
+//        AjaxResponse<Account> data = new AjaxResponse<Account>();
 
         AccountDAO accountDAO = new AccountDAO();
         boolean result = accountDAO.checkLogin(account.getEmail(), account.getPassword());
 
         System.out.println(account.getEmail());
         System.out.println(account.getPassword());
-        if(result) {
+        if (result) {
             Object info = accountDAO.getAccount(account.getEmail());
 
             try {
-                node.set("userInfo", mapper.valueToTree(info));
+//                data.setResult("true");
+//                data.setData((Account) info);
                 node.put("result", true);
+                node.set("data", mapper.valueToTree(info));
                 session.setAttribute("result", true);
-            } catch(Exception e) {
+            } catch (Exception e) {
                 System.out.println(e);
             }
         } else {
             try {
+//                data.setResult("false");
                 node.put("result", false);
                 session.setAttribute("result", false);
-            } catch(Exception e) {
+            } catch (Exception e) {
                 System.out.println(e);
             }
         }
 
         return node.toString();
+//        return data;
     }
 
 }
